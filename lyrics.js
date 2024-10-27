d52917830aef70afa77197a092e98e0a2f7e04f3
kadir <101037506+akakadir@users.noreply.github.com>
2024-10-27 03:20:46 +0300

    Update lyrics.js

--- lyrics.js
+++ lyrics.js
@@ -2,11 +2,15 @@
 //rework / 26-10-24
 
 function embd(e) {
-    var n = document.createElement("embed");
-    n.setAttribute("src", "//youtube.com/embed/" + e.dataset.id + "?rel=0&showinfo=0&autohide=1&modestbranding=1"), e.parentNode.replaceChild(n, e);
+    var n = document.createElement("iframe");
+    n.setAttribute("src", "//youtube.com/embed/" + e.dataset.id + "?rel=0&showinfo=0&autohide=1&modestbranding=1");
+    n.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"); 
+    n.setAttribute("frameborder", "0");
+    n.setAttribute("allowfullscreen", "");
+    e.parentNode.replaceChild(n, e);
 }
 function _embd() {
-    for (var e = document.getElementsByClassName("embed"), n = 0; n < e.length; n++) {
+    for (var e = document.getElementsByClassName("single"), n = 0; n < e.length; n++) {
         var i = e[n].dataset.id,
             t = document.createElement("a");
         t.setAttribute("data-id", i);
@@ -24,8 +28,12 @@ function _embd() {
 $().html(), document.addEventListener("DOMContentLoaded", _embd);
 
 function embdpl(e) {
-    var n = document.createElement("embed");
-    n.setAttribute("src", "//youtube.com/embed/videoseries?list=" + e.dataset.id + "&rel=0&showinfo=0&autohide=1&modestbranding=1"), e.parentNode.replaceChild(n, e);
+    var n = document.createElement("iframe");
+    n.setAttribute("src", "//youtube.com/embed/videoseries?list=" + e.dataset.id + "&rel=0&showinfo=0&autohide=1&modestbranding=1");
+    n.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"); 
+    n.setAttribute("frameborder", "0");
+    n.setAttribute("allowfullscreen", "");
+    e.parentNode.replaceChild(n, e);
 }
 function _pl() {
     for (var e = document.getElementsByClassName("playlist"), n = 0; n < e.length; n++) {
